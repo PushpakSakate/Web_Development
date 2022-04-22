0983aba183581cd3036f743f5bdbcbe18ea46d4f
PushpakSakate <pushpaksakate21@gmail.com>
2022-04-22 13:44:46 +0530

    Upload

---
+++ js/JS_Practice.js
@@ -0,0 +1,16 @@
+
+       /*  function sum (a,b) {
+            var c = a + b;
+            return c;
+        }
+         var x = sum(20,30);
+         x;
+         console.log(x);
+ */
+
+         // anonymous functions
+         var x = function sum(a,b){
+             var c = a + b;
+             return c;
+         }
+         console.log(x(50,20));
